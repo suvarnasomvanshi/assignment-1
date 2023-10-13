2da07ce395857841f02e6693f098c1e80b8daa06
suvarna <somavanshisuvarna48@gmail.com>
2023-10-13 20:32:15 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+const port  = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{console.log('connected to database')})
+.catch((err)=>{console.log(err)})
+
+
+app.listen({port},()=>{
+    console.log(`listening to port ${port}`)
+})
